899f20c336d40df807c96e1186b4c82f059b9f83
CassidyCathe <cassiecathy555@gmail.com>
2024-02-25 21:48:31 -0600

    Hardskills and Formatting
    Added Ethan's code to the branch after problems on Github, and updated formatting of comments to be uniform.

--- basicClassesCoding/Job.java
+++ basicClassesCoding/Job.java
@@ -1,17 +1,18 @@
 package basicClassesCoding;
-
 public class Job {
     // class variables
     private String Title;
     private int Experience;
 
-    // constructor
+
+    // default constructor
     public Job() {
     }
 
+
     ///// MUTATORS /////
 
-    // Sets the title of the job
+    // sets the title of the job
     public void setTitle(String Title) {
         this.Title = Title;
     }
@@ -23,28 +24,29 @@ public class Job {
 
     ///// END OF MUTATORS /////
 
+
     ///// ACCESSORS /////
 
-    // Sets the title of the job
+    // retrieves title of job
     public String getTitle() {
         return Title;
     }
 
-    // Sets the lenghth of experience for the job
+    // retrieves experience on job
     public int getExperience() {
         return Experience;
     }
 
     ///// END OF ACCESSORS /////
 
+
     ///// METHODS /////
 
-    // displays the Job Title and Experience Length
+    // displays the job title and experience length
     public void displayDetails() {
         System.out.println("--- Job Details ---");
         System.out.println("Job Title: " + Title);
         System.out.println("Length of Experience: " + Experience);
         System.out.println();
     }
-
 }
